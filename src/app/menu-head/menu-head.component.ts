0412ead9eb34cc826021a13801463bcb41ef1768
vinodh1988 <k.c.vinodh@hotmail.com>
2017-11-20 20:50:11 +0530

    Child Component added

---
+++ src/app/menu-head/menu-head.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'headbar',
+  templateUrl: './menu-head.component.html',
+  styleUrls: ['./menu-head.component.css']
+})
+export class MenuHeadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
